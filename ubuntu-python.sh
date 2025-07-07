e7a5cfbc1a201683e7b39560593bcd1c75d4f6c8
rohit1101 <37110560+rohit1101@users.noreply.github.com>
2025-07-07 17:40:09 +0530

    Update ubuntu-python.sh

--- ubuntu-python.sh
+++ ubuntu-python.sh
@@ -36,3 +36,61 @@ echo "$(docker --version)" > ~/result.txt
 
 apt update -y && apt upgrade -y
 apt install python3 python3-pip -y 
+
+# Not yet tested the below 
+export NODE_EXPORTER_VERSION=node_exporter-1.9.1.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/$NODE_EXPORTER_VERSION.tar.gz
+
+# create a dedicated user and group
+sudo groupadd -f node_exporter
+sudo useradd -g node_exporter --no-create-home --shell /bin/false node_exporter
+sudo mkdir /etc/node_exporter
+sudo chown node_exporter:node_exporter /etc/node_exporter
+
+# extract the tar files
+
+tar -xvf node_exporter-1.0.1.linux-amd64.tar.gz
+mv node_exporter-1.0.1.linux-amd64 node_exporter-files
+
+# Copy node_exporter binary from node_exporter-files folder to /usr/bin and change the ownership to prometheus user.
+sudo cp node_exporter-files/node_exporter /usr/bin/
+sudo chown node_exporter:node_exporter /usr/bin/node_exporter
+
+# Create a node_exporter service file.
+
+sudo vi /usr/lib/systemd/system/node_exporter.service
+
+Add the following content:
+
+[Unit]
+Description=Node Exporter
+Documentation=https://prometheus.io/docs/guides/node-exporter/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+Restart=on-failure
+ExecStart=/usr/bin/node_exporter \
+  --web.listen-address=:9100
+
+[Install]
+WantedBy=multi-user.target
+
+sudo chmod 664 /usr/lib/systemd/system/node_exporter.service
+
+# restart the systemd service to register the prometheus service and start the prometheus service.
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+
+# check the status
+
+sudo systemctl status node_exporter
+
+# enable the service 
+sudo systemctl enable node_exporter.service|
+
+Reference: https://developer.couchbase.com/tutorial-node-exporter-setup/
